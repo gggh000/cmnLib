225b5943882eeee9014f76a3ee27404cd950a74d
Your Name <you@example.com>
2019-01-07 19:41:13 -0800

    emptied __init__py

--- cmnLib/__init__.py
+++ cmnLib/__init__.py
@@ -1,6 +1,6 @@
-from .text import joke
-from .ucs import *
-from .saLibrary import *
-from .security import *
-from .pcie import *
+#from .text import joke
+#from .ucs import *
+#from .saLibrary import *
+#from .security import *
+#from .pcie import *
 
